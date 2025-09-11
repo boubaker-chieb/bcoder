c6ab00e50af4be755e6d40bfe46c530882536445
Boubaker Chieb <boubaker.ch@outlook.fr>
2025-09-11 18:08:05 +0200

    add yellow

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ import { Home } from '../pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Home],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
